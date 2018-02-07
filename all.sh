58de85f19563970bcdeab12b1b1338a59cd34f2f
Erik Phillips <erikvip@gmail.com>
2018-02-07 05:00:32 -0700

    Moved log directory verification to all.sh script instead of xdcc browser

--- all.sh
+++ all.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+# Default location for KVIrc
+LOG_DIR="${HOME}/.config/KVIrc/log";
+LOG_DIR_WINDOWS="$APPDATA\KVIrc4\log"
 BASEPATH=$(realpath `dirname $0`);
-FILEDIR=${1:-"$BASEPATH"};
 
-(>&2 echo "Processing all channel_*.log files from ${FILEDIR}")
+# Use the windows path if we're on Cygwin
+[ `uname -o` == "Cygwin" ] && LOG_DIR="${LOG_DIR_WINDOWS}"; 
+
+# If $1 was passed, and is a valid path, use it instead
+[ ! -z "${1}" ] && LOG_DIR="${1}"; 
+
+# Verify the log directory exists
+[ ! -d "${LOG_DIR}" ] && echo "Error: could not locate log file directory at ${LOG_DIR}" && exit 1; 
+
+(>&2 echo "Processing all channel_*.log files from ${LOG_DIR}")
 
 process() {
     f="$1"
@@ -26,7 +37,7 @@ export BASEPATH
 # Run the awk script once with no input, to output the row header. All subseqent calls will have SKIPHEADER set
 awk -f "${BASEPATH}/import.awk" < /dev/null
 
-find "${FILEDIR}" -maxdepth 1 -type f -ctime -5 -iname 'channel_*.log' -exec bash -c 'process "$0"' {} \;
+find "${LOG_DIR}" -maxdepth 1 -type f -ctime -5 -iname 'channel_*.log' -exec bash -c 'process "$0"' {} \;
 
 (>&2 echo )
 
