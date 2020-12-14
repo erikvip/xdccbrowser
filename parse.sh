693dc639e67f898cb60ad78eee039db2aece802a
Erik Phillips <erikvip@gmail.com>
2020-12-13 18:21:24 -0800

    Fix for bracket in nicknames

--- parse.sh
+++ parse.sh
@@ -13,7 +13,7 @@ BASEDIR=$(dirname $0);
 tr -dc "_A-Z-a-z-0-9 :#_\-\.\n\[\]\|" | cut -d ' ' -f2- | egrep -v ' has (joined|left|quit)' | egrep '^[^ ]* \#[0-9]+' \
     | grep -vi '.German.' \
     | grep -vi '.French.' \
-	| sed -r -e 's/\[([ 0-9\.MGKmgk]*)\]/\1/g' -e 's/ \#([0-9]*) / \1 /g' -e 's/ +([0-9]+)x/ \1/g' \
+	| sed -r -e 's/ \[([ 0-9\.MGKmgk]*)\] / \1 /g' -e 's/ \#([0-9]*) / \1 /g' -e 's/ +([0-9]+)x/ \1/g' \
     | sort | uniq \
     | awk -v NETWORK="${NETWORK}" -v CHANNEL="${CHANNEL}" -v SKIPHEADER="${SKIPHEADER}" -F ' ' -f "${BASEDIR}/import.awk"
 
